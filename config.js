3955dba9ec07ea11acffaaf5860def84270f126d
XNOR12-arch2 <sagarikajayamini1980@gmail.com>
2024-12-02 07:55:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '94703090240'
+global.nomerowner = ["94703090240"]
 
 // Apikey 
 
